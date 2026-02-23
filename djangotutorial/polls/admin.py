5017e22775bc1f8174921a7399a9e5b885919cac
Killian Morere <killian.morere@fms-ea.com>
2026-02-23 15:41:08 +0100

    Added Admin functions with simple filters.

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "published_date"]
+    list_filter = ["question_text", "published_date"]
+    ordering = ["published_date"]
+    search_fields = ["question_text"]
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ["question", "choice_text", "votes"]
+    list_filter = ["question"]
+    ordering = ["question", "votes"]
+    search_fields = ["question", "choice_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
