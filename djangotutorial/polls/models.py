75cc6ad6bbde2b522d6e0f53e7da6236e0cbd96f
Killian Morere <killian.morere@fms-ea.com>
2026-02-24 12:02:37 +0100

    Added functions age, get_choices and get_max_choices for ex 2.2.3.

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -9,10 +9,22 @@ class Question(models.Model):
     published_date = models.DateTimeField("published date")
 
     def __str__(self):
-        return self.question_text
+        return self.question_text[:20] + f" {self.published_date}"
 
     def is_recent(self):
         return self.published_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def age(self):
+        time_elapsed = datetime.datetime.now(datetime.timezone.utc) - self.published_date
+        return (f"{time_elapsed.days} days",
+                f"{time_elapsed.seconds // 3600} minute(s)",
+                f"{(time_elapsed.seconds // 60) % 60} second(s).")
+
+    def get_choices(self):
+        return Choice.objects.filter(question=self)
+
+    def get_max_choices(self):
+        return Choice.objects.filter(question=self).aggregate(models.Max("votes"))
 
 
 class Choice(models.Model):
@@ -21,4 +33,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text
+        return self.choice_text + f" {self.votes}"
