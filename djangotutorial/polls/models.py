d83627cf4c3e0200c58b626f75608e9b0731465d
Killian Morere <killian.morere@fms-ea.com>
2026-02-24 13:02:36 +0100

    Added docstrings for the Question functions.

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -12,18 +12,33 @@ class Question(models.Model):
         return self.question_text[:20] + f" {self.published_date}"
 
     def is_recent(self):
+        """
+        Is the question's published date less than a day old.
+        :return: True if it is less than a year old.
+        """
         return self.published_date >= timezone.now() - datetime.timedelta(days=1)
 
     def age(self):
+        """
+        :return: The time elapsed between today and the published date.
+        """
         time_elapsed = datetime.datetime.now(datetime.timezone.utc) - self.published_date
         return (f"{time_elapsed.days} days",
                 f"{time_elapsed.seconds // 3600} minute(s)",
                 f"{(time_elapsed.seconds // 60) % 60} second(s).")
 
     def get_choices(self):
+        """
+        Get the choices related to the question.
+        :return: All the choices related.
+        """
         return Choice.objects.filter(question=self)
 
     def get_max_choices(self):
+        """
+        Get the highest amount of votes for a question.
+        :return: The highest number of votes.
+        """
         return Choice.objects.filter(question=self).aggregate(models.Max("votes"))
 
 
