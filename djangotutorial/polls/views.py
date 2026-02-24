0f003be12a4a5b15f0b0c64b8d8157d5f19ab2d6
Killian Morere <killian.morere@fms-ea.com>
2026-02-24 15:54:07 +0100

    [Part 3] Added links to questions in the polls page.

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_questions = Question.objects.order_by("-published_date")[:5]
+    output = ", ".join([q.question_text for q in latest_questions])
+    context = {"latest_questions": latest_questions}
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at the question %s." % question_id)
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
