9b5814e10b3f41cf99e0617e20c28a77ec7c6609
Killian Morere <killian.morere@fms-ea.com>
2026-02-26 15:43:32 +0100

    Added custom styling to the admin page.

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -2,11 +2,22 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ["question_text", "published_date"]
-    list_filter = ["question_text", "published_date"]
+    list_filter = ["published_date"]
     ordering = ["published_date"]
     search_fields = ["question_text"]
+
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["published_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInLine]
 
 
 class ChoiceAdmin(admin.ModelAdmin):
