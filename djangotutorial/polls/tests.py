034683d73e52e265ac96ae4d0992fcc39bab569c
Killian Morere <killian.morere@fms-ea.com>
2026-02-26 14:34:48 +0100

    Made tests for is_recent for the expected result and future.

--- djangotutorial/polls/tests.py
+++ djangotutorial/polls/tests.py
@@ -7,6 +7,11 @@ from .models import Question
 
 class QuestionTests(TestCase):
     def test_is_recent_future(self):
-        time = timezone.now() + datetime.timedelta(days=7)
+        time = timezone.now() + datetime.timedelta(days=1, seconds=1)
         future_question = Question(published_date=time)
         self.assertIs(future_question.is_recent(), False)
+
+    def test_is_recent(self):
+        time = timezone.now() + datetime.timedelta(hours=23, minutes=59, seconds=59)
+        future_question = Question(published_date=time)
+        self.assertIs(future_question.is_recent(), True)
