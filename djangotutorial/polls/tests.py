141a0f1d6ff00c80501d63950de77c68f60da2bc
Killian Morere <killian.morere@fms-ea.com>
2026-02-26 14:27:18 +0100

    Created tests for is_recent function in Question model.

--- djangotutorial/polls/tests.py
+++ djangotutorial/polls/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+import datetime
+from .models import Question
+
+
+class QuestionTests(TestCase):
+    def test_is_recent(self):
+        time = timezone.now() + datetime.timedelta(days=1)
+        future_question = Question(published_date=time)
+        self.assertIs(future_question.is_recent(), False)
