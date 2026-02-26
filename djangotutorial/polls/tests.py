f46368431363853f949241d79024232fe2962bf2
Killian Morere <killian.morere@fms-ea.com>
2026-02-26 15:07:39 +0100

    Added tests for the DetailView.

--- djangotutorial/polls/tests.py
+++ djangotutorial/polls/tests.py
@@ -79,3 +79,23 @@ class QuestionIndexTests(TestCase):
         question2 = create_question(question_text="Past 2", days=-14)
         response = self.client.get(reverse("polls:index"))
         self.assertQuerySetEqual(response.context["latest_questions"], [question2, question1])
+
+
+class QuestionDetailTests(TestCase):
+    def test_future_question(self):
+        """
+        Test for a question in the future.
+        """
+        future_question = create_question(question_text="Future.", days=7)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        Test for a question in the past.
+        """
+        past_question = create_question(question_text="Past.", days=-7)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
